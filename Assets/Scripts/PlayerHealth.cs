1ae1113d95566230928614bf9c42007bd2a81462
EricW <lee21acc973@163.com>
2018-02-21 19:34:10 +1100

    adjust enemy movement, add Health Bar, damage effect, Lose UI

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public float currentHealth;
-    public float maxHealth;
+    public float currentHealth = 100;
+    public float maxHealth = 100;
+    public float damage = 10;
+    public Image hpBar;
+    public GameObject redPanel;
+    public GameObject losePanel;
+    private bool isHurt = false;
 
     // Use this for initialization
     void Start ()
     {
+        Time.timeScale = 1;
+        losePanel.SetActive(false);
         maxHealth = 100;
         //reset health to full on game load
         currentHealth = maxHealth;
@@ -18,15 +27,38 @@ public class PlayerHealth : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-		
+        if (!isHurt)
+        {
+            redPanel.SetActive(false);
+        }
+        else
+        {
+            redPanel.SetActive(true);
+            isHurt = false;
+        }
+
+        if (hpBar.fillAmount <= 0)
+        {
+            losePanel.SetActive(true);
+            Time.timeScale = 0;
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); 
+            }
+        }
         
 	}
 
-    //void OnCollisionEnter(Collider enemy)
-    //{
-    //    if (enemy.gameObject.tag == "Enemy")
-    //    {
-    //        currentHealth -= enemy.attackDamage;
-    //    }
-    //}
+    void OnCollisionEnter(Collision enemy)
+    {
+        
+        if (enemy.gameObject.tag == "Enemy")
+        {
+            isHurt = true;
+            currentHealth -= damage;
+            hpBar.fillAmount = currentHealth / maxHealth;
+            
+        }
+    }
 }
