3e73106738de49ccfa71d94401ebf0ee8a09633d
EricW <lee21acc973@163.com>
2018-02-24 14:20:09 +1100

    add enemy wandering

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public GameObject ring;
+    public float destroyDelay = 1.5f;
 
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag == ("Enemy"))
         {
-            Destroy(this.gameObject, 1f);
-
+            //attach bullet on enemy
+            transform.parent = other.gameObject.transform;
+            Destroy(this.gameObject, destroyDelay - 0.5f);
+            //instantiate portal to capture enemy
             GameObject ringClone = Instantiate(ring, other.transform.position, Quaternion.identity);
-            Destroy(ringClone, 1.5f);
-            Destroy(other.transform.parent.gameObject, 1.5f);
-            Destroy(other.gameObject, 1.5f);
+            Destroy(ringClone, destroyDelay);
+            Destroy(other.transform.parent.gameObject, destroyDelay);
+            Destroy(other.gameObject, destroyDelay);
         }
     } 
 }
--- Assets/Scripts/FollowTarget.cs
+++ Assets/Scripts/FollowTarget.cs
@@ -6,28 +6,75 @@ using UnityEngine.AI;
 public class FollowTarget : MonoBehaviour
 {
     public int dis = 50;
+    public float radius = 8f;
+    public float wanderSpeedRate = 0.3f;
+    public GameObject target;
+
     private NavMeshAgent agent;
+    private bool isWander = true;
+    private Vector3 randomDir;
+    private float distance;
+    private float agentSpeed;
 
     // Use this for initialization
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        agentSpeed = agent.speed;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //GameObject target = GameObject.FindGameObjectWithTag("Player");
         // update player's position
-        GameObject target = GameObject.FindGameObjectWithTag("Player");
         Vector3 targetPosition = target.transform.position;
-        float distance = Vector3.Distance(transform.position, targetPosition);
+        distance = Vector3.Distance(transform.position, targetPosition);
 
         // move enemy towards player if player close enough
         if (distance < dis)
         {
-            
+            // rotate enemy towards player
             transform.LookAt(target.transform);
+            agent.speed = agentSpeed;  //reset speed to full speed
             agent.SetDestination(targetPosition);
         }
+
+        if (distance > dis)
+        {
+            if (isWander)
+            {
+                GetRandomDirection();
+                Debug.Log(randomDir);
+                isWander = false;
+            }
+
+            if (isWander == false)
+            {
+                agent.speed = agentSpeed * wanderSpeedRate;  //set speed to wander speed
+                agent.SetDestination(randomDir);
+            }
+            
+            if (Vector3.Distance(transform.position, randomDir) < 0.5f)
+            {
+                isWander = true;
+            }
+        }
     }
+
+    void GetRandomDirection()
+    {
+        float randX = Random.Range(-radius, radius);
+        float randZ = Random.Range(-radius, radius);
+        // Create a random direction vector
+        randomDir = new Vector3(randX, 0, randZ) + transform.position;  
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Environment" && distance > dis)
+        {
+            GetRandomDirection();  //generate a new wander direction once hit the wall
+        }
+    } 
 }
